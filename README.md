1b64c9dda859f024ac08d822f7618678c4468ae6
Michael L Heuer <heuermh@acm.org>
2022-11-07 15:21:08 -0600

    add script from 2020

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # minnesota-2022-election-results
+
+`wget` followed by `git commit` via `cron` job.
+
+#### Updates
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+wget \
+    --mirror \
+    --no-parent \
+    "https://electionresults.sos.state.mn.us/Results/Index?ersElectionId=136&scenario=AllwithoutJudicial&electionDate=11%2F3%2F2020%2012%3A00%3A00%20AM"
+
+wget \
+    --mirror \
+    --no-parent \
+    "https://electionresults.sos.state.mn.us/Results/Index?ersElectionId=136&scenario=USRepresentative"
+
+wget \
+    --mirror \
+    --no-parent \
+    "https://electionresults.sos.state.mn.us/Select/MediaFiles/Index?ersElectionId=136"
+
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/USPres.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/USPresCG.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/USPresLG.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/USPresCty.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/USPresPct.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/ussenate.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/ussenatecg.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/ussenatecty.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/ussenatepct.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/ushouse.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/USHouseCty.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/ushousepct.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/stsenate.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/StateSenateByPct.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/LegislativeByDistrict.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/sthousepct.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/allracesbycounty.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/allracesbyprecinct.txt
+wget --mirror https://electionresultsfiles.sos.state.mn.us/20201103/pctstats.txt
+
+TIMESTAMP=`date`
+git commit -a -m "Update at $TIMESTAMP"
