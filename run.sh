aac53f8538d1a1fefe5cb1e2321f74642871d724
Michael L Heuer <heuermh@acm.org>
2022-11-08 18:59:09 -0600

    Update at Tue Nov  8 18:59:09 CST 2022

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#wget \
-#    --mirror \
-#    --no-parent \
-#    "https://electionresults.sos.state.mn.us/Results/Index?ersElectionId=136&scenario=AllwithoutJudicial&electionDate=11%2F3%2F2020%2012%3A00%3A00%20AM"
-#
+wget \
+    --mirror \
+    --no-parent \
+    https://electionresults.sos.state.mn.us/Results/Index?ersElectionId=149&scenario=AllwithoutJudicial&electionDate=11%2F8%2F2022%2012%3A00%3A00%20AM
+
 #wget \
 #    --mirror \
 #    --no-parent \
